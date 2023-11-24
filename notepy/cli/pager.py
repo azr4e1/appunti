82b9e2feba020d8978bf76a625a56e5dd13e0df8
Lorenzo Drumond <drumondlorenzo@posteo.net>
2023-11-24 15:32:46 +0000

    improved pager to support following links

--- notepy/cli/pager.py
+++ notepy/cli/pager.py
@@ -1,5 +1,7 @@
 import curses
 from notepy.zettelkasten import Note, Zettelkasten
+from notepy.zettelkasten.notes import sluggify
+from typing import Optional
 from pathlib import Path
 from dataclasses import fields
 import textwrap
@@ -8,13 +10,21 @@ import textwrap
 LINKS_RATIO = 4
 
 
-root = Path("/home/ld/Desktop/Knowledge")
-file = "4c8579262e8f19ddd7b3ee404821b236.md"
-filename = root / file
-content = filename.read_text()
-parsing_objs = [note_obj.name for note_obj in fields(Note) if note_obj not in ['link', 'body']]
-note = Note.read(filename, parsing_objs)
-zk = Zettelkasten("/home/ld/Desktop/Knowledge", "Lorenzo Drumond")
+zk = Zettelkasten("/home/lorenzo/Desktop/Knowledge", "Lorenzo Drumond")
+
+
+def get_id_from_link(link, zk: Zettelkasten) -> Optional[str]:
+    results = zk.list_notes(show=['title', 'zk_id'])
+    titles = [sluggify(title) for title, _ in results]
+    with open("/home/lorenzo/log2", "a") as f:
+        f.write(str(titles)+"\n")
+        f.write(link + "\n")
+
+    try:
+        position = titles.index(link)
+        return results[position][1]
+    except ValueError:
+        return None
 
 
 def main(screen):
@@ -27,11 +37,16 @@ def main(screen):
         screen.refresh()
         win_width = curses.COLS // LINKS_RATIO
         win = curses.newwin(curses.LINES, win_width, 0, curses.COLS-win_width)
-        win.border("|", " ", " ", " ", " ", " ", " ", " ")
+        win.border("|", " ", " ", " ", "|", " ", "|", " ")
         win.refresh()
         links_pos = 0
         links = []
-        actual_links = note.links
+        root = Path("/home/lorenzo/Desktop/Knowledge")
+        file = "4c8579262e8f19ddd7b3ee404821b236.md"
+        filename = root / file
+        parsing_objs = [note_obj.name for note_obj in fields(Note) if note_obj not in ['link', 'body']]
+        note = Note.read(filename, parsing_objs)
+        actual_links = list(note.links)
         for index, link in enumerate(actual_links):
             link = f"[{index+1}] " + link
             wrapped_links = textwrap.wrap(link, win_width-2)
@@ -71,10 +86,10 @@ def main(screen):
             elif c == ord('g'):
                 pos = 0
             elif c == ord('G'):
-                pos = len(lines)
-                screen.clear()
-                screen.refresh()
-            if c == ord('J'):
+                pos = len(lines) - curses.LINES
+                # screen.clear()
+                # screen.refresh()
+            elif c == ord('J'):
                 links_pos += 1
             elif c == ord('K'):
                 links_pos -= 1
@@ -87,7 +102,7 @@ def main(screen):
                 screen.refresh()
                 win_width = curses.COLS // LINKS_RATIO
                 win = curses.newwin(curses.LINES, win_width, 0, curses.COLS-win_width)
-                win.border("|", " ", " ", " ", " ", " ", " ", " ")
+                win.border("|", " ", " ", " ", "|", " ", "|", " ")
                 links = []
                 for index, link in enumerate(actual_links):
                     link = f"[{index+1}] " + link
@@ -110,6 +125,54 @@ def main(screen):
                     pad.addstr(index, 0, line)
                 screen.clear()
                 screen.refresh()
+
+            else:
+                for index, link in enumerate(links):
+                    if index+1 >= 10:
+                        continue
+                    if c == ord(str(index+1)):
+                        new_id = get_id_from_link(actual_links[index], zk)
+                        if new_id is None:
+                            continue
+                        pos = 0
+                        links_pos = 0
+                        filename = root / Path(new_id).with_suffix(".md")
+                        note = Note.read(filename, parsing_objs)
+                        screen.clear()
+                        screen.refresh()
+                        win_width = curses.COLS // LINKS_RATIO
+                        win = curses.newwin(curses.LINES, win_width, 0, curses.COLS-win_width)
+                        win.border("|", " ", " ", " ", "|", " ", "|", " ")
+                        win.refresh()
+                        links_pos = 0
+                        links = []
+                        actual_links = list(note.links)
+                        for index, link in enumerate(actual_links):
+                            link = f"[{index+1}] " + link
+                            wrapped_links = textwrap.wrap(link, win_width-2)
+                            for wl in wrapped_links:
+                                links.append(wl)
+                        links_pad = curses.newpad(len(links)+1, win_width-1)
+                        for index, link in enumerate(links):
+                            links_pad.addstr(index, 1, link+"\n")
+                        # links_pad.refresh(links_pos, 0, 0, curses.COLS-win_width+1, curses.LINES, curses.COLS)
+
+                        pos = 0
+                        lines = []
+                        simple_line = note.materialize().split("\n")
+                        for line in simple_line:
+                            if line == "":
+                                lines.append("\n")
+                            else:
+                                for wrapped_line in textwrap.wrap(line, curses.COLS-win_width-1):
+                                    lines.append(wrapped_line)
+                        # lines = note.body.split("\n")
+                        pad = curses.newpad(len(lines)+1, curses.COLS-win_width)
+                        for index, line in enumerate(lines):
+                            pad.addstr(index, 0, line)
+
+                        # pad.refresh(pos, 0, 0, 0, curses.LINES, curses.COLS-win_width-1)
+                        break
 
             if pos < 0:
                 pos = 0
